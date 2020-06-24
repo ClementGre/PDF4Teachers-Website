d28d30f3c60bf28178f6e80ed558e3f792141191
Clément GRENNERAT <clement.grennerat@free.fr>
2020-06-24 18:32:39 +0200

    Setup Project + Translations Feature
    Création des fichiers de base du site web et mise en place d'un système 
    qui permet la traduction du texte dans plusieurs languages (Créé en 
    PHP).

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php require "php/translator.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<!--          PAGE INFO          -->
+<head>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="icon" href="data/img/logo.png" />
+	<title>PDF4Teachers - Site Officiel | Accueil</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato:100,300,regular,700,900' type='text/css'>
+
+	<link rel="stylesheet" type="text/css" href="css/header.css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link rel="stylesheet" type="text/css" href="css/footer.css" />
+
+</head>
+
+
+<body>
+	<div class="filtre"></div>
+
+
+<!--          HEADER          -->
+	<header>
+		<?php include 'header.php'; ?>
+	</header>
+
+
+<!--          MAIN          -->
+	<main>
+			<p>
+				<?= t("message.test") ?>
+			</p>
+	</main>
+
+
+<!--          FOOTER          -->
+	<footer>
+		<?php include 'footer.php'; ?>
+	</footer>
+
+	<script type='text/javascript' src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+</html>
---
+++ php/translator.php
@@ -0,0 +1,67 @@
+<?php
+
+setcookie('language', 'fr');
+
+function getBrowserLanguage( $available = [], $default = 'en' ) {
+	if(isset($_SERVER[ 'HTTP_ACCEPT_LANGUAGE' ])){
+
+		$langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+    if(empty($available)){
+      return $langs[0];
+    }
+		foreach($langs as $lang){
+			$lang = substr($lang, 0, 2);
+			if(in_array($lang, $available)){
+				return $lang;
+			}
+		}
+	}
+	return $default;
+}
+$languages = ['fr', 'en'];
+
+// Detect language with cookie or navigator language
+if(isset($_COOKIE['language'])){
+	if(in_array($_COOKIE['language'], $languages)){
+		$language = $_COOKIE['language'];
+	}else{
+		$language = getBrowserLanguage($languages);
+	}
+}else{
+		$language = getBrowserLanguage($languages);
+}
+
+// Load translation file
+$translations = array();
+$translationsFile = realpath('translations/' . $language . '.properties');
+
+foreach(file($translationsFile, true) as $line){
+	$translations = array_merge($translations, array(explode('=', $line, 2)[0] => explode('=', $line, 2)[1]));
+}
+
+if(realpath('commonTranslations/' . $language . '.properties') != false){
+	foreach(file(realpath('commonTranslations/' . $language . '.properties'), true) as $line){
+		$translations = array_merge($translations, array(explode('=', $line, 2)[0] => explode('=', $line, 2)[1]));
+	}
+}else if(realpath('../commonTranslations/' . $language . '.properties') != false){
+	foreach(file(realpath('../commonTranslations/' . $language . '.properties'), true) as $line){
+		$translations = array_merge($translations, array(explode('=', $line, 2)[0] => explode('=', $line, 2)[1]));
+	}
+}else{
+	echo "Erreur : Impossible de trouver le dossier commonTranslations";
+}
+
+
+// Translate a sentence
+function t($key){
+	global $translations;
+
+  if(isset($translations[$key])){
+    return $translations[$key];
+  }else{
+		return $key;
+	}
+}
+
+?>
