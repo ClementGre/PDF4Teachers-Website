5408e26ff27d24429cba8fa01d7bf17237399ced
ClementGre <clement.grennerat@free.fr>
2021-10-09 16:37:49 +0200

    Add vector-elements screenshot

--- js/home.js
+++ js/home.js
@@ -1,23 +1,23 @@
 $(document).ready(() => {
 
+    const previewImages = ["data/small-img/shots/text-elements.png", "data/small-img/shots/grading.png", "data/small-img/shots/vector-elements.png"]
+
     // Switch preview feature
     const interval = 10;
-    let status = 1;
+    let status = 0;
     setInterval(() => {
         const img = $('.preview-block img');
         img.addClass("hidden");
 
         setTimeout(() => {
-            if(status === 1){
-                img.attr("src", "data/small-img/shots/grading.png")
-            }else{
-                img.attr("src", "data/small-img/shots/text-elements.png")
+            img.attr("src", previewImages[status])
+            img.removeClass("hidden");
+
+            status++;
+            if(status >= previewImages.length){
                 status = 0;
             }
-            status++;
-            img.removeClass("hidden");
         }, 300);
-
 
     }, 1000 * interval);
 
