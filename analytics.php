72cd73100c542469e90ced8c04528ef3d4566aa5
Clément Grennerat <clement.grennerat@free.fr>
2021-10-13 21:20:55 +0200

    Switch from Google analytics to Matomo (local)

--- analytics.php
+++ analytics.php
@@ -1,4 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-DMTJ2G00CR"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -7,3 +7,6 @@
 
   gtag('config', 'G-DMTJ2G00CR');
 </script>
+-->
+
+<!-- Matomo --> <script> var _paq = window._paq = window._paq || []; /* tracker methods like "setCustomDimension" should be called before "trackPageView" */ _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u="//html.pdf4teachers.org/matomo/"; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); })(); </script> <!-- End Matomo Code -->
