8b75cc1bbe58d479ad9ccd939e3564cb70d58f6a
Clement Grennerat <clement.grennerat@free.fr>
2022-08-27 13:39:51 +0000

    migrate to new nomenclature

--- Statistics/downloadStats.php
+++ Statistics/downloadStats.php
@@ -2,7 +2,7 @@
 require("../php/database.php");
 
 // get stats
-$query = $db->query("SELECT * FROM stats ORDER BY date DESC");
+$query = $db->query("SELECT * FROM app_start_updates ORDER BY date DESC");
 
 $users = array();
 while($row = $query->fetch(PDO::FETCH_ASSOC)){
@@ -13,7 +13,7 @@ header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=PDF4Teachers-Statistics.csv');
 $output = fopen('php://output', 'w');
 
-fputcsv($output, array('id', 'time', 'starts', 'version', 'date', 'language'));
+fputcsv($output, array('id', 'date', 'version', 'language', 'time', 'starts'));
 if(count($users) > 0){
     foreach($users as $row){
         fputcsv($output, $row);
--- analytics.php
+++ analytics.php
@@ -1,15 +1,16 @@
-<!-- Global site tag (gtag.js) - Google Analytics
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DMTJ2G00CR"></script>
+<!-- Matomo -->
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-DMTJ2G00CR');
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//html.pdf4teachers.org/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
--->
-<!-- Matomo Tag Manager --> <script> var _mtm = window._mtm = window._mtm || []; _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'}); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.async=true; g.src='https://html.pdf4teachers.org/matomo/js/container_CSVTHDPx.js'; s.parentNode.insertBefore(g,s); </script> <!-- End Matomo Tag Manager -->
-
-<!-- Matomo --> <script> var _paq = window._paq = window._paq || []; /* tracker methods like "setCustomDimension" should be called before "trackPageView" */ _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u="//html.pdf4teachers.org/matomo/"; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); })(); </script> <!-- End Matomo Code -->
-
+<!-- End Matomo Code -->
 
