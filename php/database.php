c95c12b306438e0dfe5b887d161d2ee16f3664f7
Clément Grennerat <clement.grennerat@free.fr>
2023-01-11 15:17:12 +0100

    Update password env var name
    Took 18 seconds
    
    
    Took 4 seconds

--- php/database.php
+++ php/database.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$db = new PDO('mysql:host=localhost;dbname=pdf4teachers','pdf4teachers', getenv("PDF4TEACHERS_DB_PASS"));
+		$db = new PDO('mysql:host=localhost;dbname=pdf4teachers','pdf4teachers', getenv("PDF4TEACHERS_DB_PWD"));
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(PDOException $e){
 		echo "Erreur de connexion à la base de donnée.<br> Erreur : " . $e;
